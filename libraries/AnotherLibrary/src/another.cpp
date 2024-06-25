80c987f4b9f29981e82f22c3370b4b6af2402f08
1ndevelopment <a1ndevelopment@outlook.com>
2024-06-25 00:01:42 +0000

    Initial Commit

---
+++ libraries/AnotherLibrary/src/another.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "things.h"
+
+namespace sm
+{
+    namespace lbrAnother
+    {
+        void printSomething()
+        {
+            std::cout << "ololo, " << someString << std::endl;
+        }
+    }
+}
---
+++ libraries/SomeLibrary/src/some.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "things.h"
+
+namespace sm
+{
+    namespace lbrSome
+    {
+        void printSomething()
+        {
+            std::cout << "ololo, " << someString << std::endl;
+        }
+    }
+}
